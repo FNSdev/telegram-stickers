c843770f1e7e10cdc096dd8de5da25aedeb55d6f
Uladzislau Stasheuski <fnsdevelopment@gmail.com>
2020-01-26 23:07:10 +0300

    Try to use K8S api to deploy #13

--- deploy/stage/create_env_secrets.sh
+++ deploy/stage/create_env_secrets.sh
@@ -1,11 +1,20 @@
-status_code=$(curl -X PUT --write-out %{http_code} -H "Content-Type: application/yaml" -H "Authorization: Bearer $K8S_TOKEN" --data-binary @env-secrets.yaml $K8S_URL/api/v1/namespaces/default/secrets/environment)
+status_code=$(curl -X PUT --write-out %{http_code} --silent --output /dev/null -H "Content-Type: application/yaml" -H "Authorization: Bearer $K8S_TOKEN" --data-binary @env-secrets.yaml $K8S_URL/api/v1/namespaces/default/secrets/environment)
 
 if [[ "$status_code" -eq 404 ]] ; then
   echo "Secret does not exist. Creating ..."
-  curl -X POST -H "Content-Type: application/yaml" -H "Authorization: Bearer $K8S_TOKEN" --data-binary @env-secrets.yaml $K8S_URL/api/v1/namespaces/default/secrets
-elif [[ "$status_code" -ne 200 ]] &&  [[ "$status_code" -ne 201 ]]; then
-  echo "An error occured when creating secret"  # TODO echo response body
+elif [[ "$status_code" -ne 200 ]] &&  [[ "$status_code" -ne 201 ]] ; then
+  echo "An error occured when updating secret"  # TODO echo response body
   exit 1
+else
+  echo "Secret updated successfully"
+  exit 0
+fi
+
+status_code=$(curl -X POST --write-out %{http_code} --silent --output /dev/null -H "Content-Type: application/yaml" -H "Authorization: Bearer $K8S_TOKEN" --data-binary @env-secrets.yaml $K8S_URL/api/v1/namespaces/default/secrets)
+
+if [[ "$status_code" -ne 200 ]] &&  [[ "$status_code" -ne 201 ]] ; then
+  echo "An error occured when updating secret"  # TODO echo response body
+  exit 2
 else
   echo "Secret created successfully"
   exit 0
