68ea939f04f79cfde7dde9851083436bf3eb89ae
Uladzislau Stasheuski <fnsdevelopment@gmail.com>
2020-01-26 22:49:04 +0300

    Try to use K8S api to deploy #10

--- deploy/stage/create_env_secrets.sh
+++ deploy/stage/create_env_secrets.sh
@@ -1,7 +1,11 @@
 status_code=$(curl -X PUT --write-out %{http_code} --silent --output /dev/null -H "Content-Type: application/yaml" -H "Authorization: Bearer $K8S_TOKEN" --data-binary @env-secrets.yaml $K8S_URL/api/v1/namespaces/default/secrets/environment)
 
-if [[ "$status_code" e 404 ]] ; then
+echo $status_code
+
+if [[ "$status_code" -eq 404 ]] ; then
   curl -X POST -H "Content-Type: application/yaml" -H "Authorization: Bearer $K8S_TOKEN" --data-binary @env-secrets.yaml $K8S_URL/api/v1/namespaces/default/secrets
+elif [[ "$status_code" -ne 200 ]] &&  [[ "$status_code" -ne 201 ]]
+  exit 1
 else
   exit 0
 fi
