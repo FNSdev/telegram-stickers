04f210ec989d2ba94b1cbb8e7169999cb6c38460
Uladzislau Stasheuski <fnsdevelopment@gmail.com>
2020-02-01 18:15:05 +0300

    Updated CI/CD

---
+++ deploy/stage/update_telegram_stickers_deployment.sh
@@ -0,0 +1,13 @@
+status_code=$(curl -X PATCH --write-out %{http_code} --silent --output /dev/null \
+  -H "Content-Type: strategic-merge-patch+json" -H "Authorization: Bearer $K8S_TOKEN" \
+  --data {"spec":{"template":{"spec":{"containers":[{"name":"telegram-stickers","image":"fnsdev/telegram-stickers-stage:$RELEASE_VERSION"}]}}}} \
+  $K8S_URL/apis/apps/v1/namespaces/default/deployments/telegram-stickers-deployment) \
+  /
+
+if [[ "$status_code" -ne 200 ]] &&  [[ "$status_code" -ne 201 ]] ; then
+  echo "An error occured when updating telegram_stickers_deployment"  # TODO echo response body
+  exit 1
+else
+  echo "telegram_stickers_deployment updated successfully"
+  exit 0
+fi
--- telegram_stickers/telegram_stickers/settings_local.py
+++ telegram_stickers/telegram_stickers/settings_local.py
@@ -6,5 +6,7 @@ DEBUG = True
 # TODO move allowed hosts to env variable
 ALLOWED_HOSTS = [
     '127.0.0.1',
+    'fnswebstore.xyz',
+    'www.fnswebstore.xyz',
     # '192.168.100.31',
 ]
