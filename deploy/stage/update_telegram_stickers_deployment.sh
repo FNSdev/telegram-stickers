5293ff0642cefc58173c37a4263f9a73146dff45
Uladzislau Stasheuski <fnsdevelopment@gmail.com>
2020-02-01 18:28:19 +0300

    Fixed update_deployment bash script

--- deploy/stage/update_telegram_stickers_deployment.sh
+++ deploy/stage/update_telegram_stickers_deployment.sh
@@ -1,8 +1,7 @@
 status_code=$(curl -X PATCH --write-out %{http_code} --silent --output /dev/null \
   -H "Content-Type: strategic-merge-patch+json" -H "Authorization: Bearer $K8S_TOKEN" \
   --data {"spec":{"template":{"spec":{"containers":[{"name":"telegram-stickers","image":"fnsdev/telegram-stickers-stage:$RELEASE_VERSION"}]}}}} \
-  $K8S_URL/apis/apps/v1/namespaces/default/deployments/telegram-stickers-deployment) \
-  /
+  $K8S_URL/apis/apps/v1/namespaces/default/deployments/telegram-stickers-deployment)
 
 if [[ "$status_code" -ne 200 ]] &&  [[ "$status_code" -ne 201 ]] ; then
   echo "An error occured when updating telegram_stickers_deployment"  # TODO echo response body
